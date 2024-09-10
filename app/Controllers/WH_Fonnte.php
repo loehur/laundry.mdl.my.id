bc6373d10d598e5a28956253943e69c909a1705d
loehur <loehur@gmail.com>
2024-09-10 12:55:18 +0700

    webhook error22223

--- app/Controllers/WH_Fonnte.php
+++ app/Controllers/WH_Fonnte.php
@@ -7,7 +7,6 @@ class WH_Fonnte extends Controller
       header('Content-Type: application/json; charset=utf-8');
 
       $json = file_get_contents('php://input');
-      $this->write($json);
 
       $data = json_decode($json, true);
       $id = $data['id'];
