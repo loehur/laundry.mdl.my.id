acbde02e6275d701852ff30f5bed0fdb2a9075c0
loehur <loehur@gmail.com>
2024-09-18 19:18:44 +0700

    kinerjass22322222332

--- app/Controllers/Gaji.php
+++ app/Controllers/Gaji.php
@@ -14,7 +14,6 @@ class Gaji extends Controller
 
       $user['id'] = 0;
       $user['kasbon'] = 0;
-      $data_operasi = ['title' => 'Gaji Bulanan - Rekap'];
       $dataTanggal = [];
       $dataGajiLaundry = [];
 
@@ -31,26 +30,25 @@ class Gaji extends Controller
          $date = date('Y-m');
       }
 
+      $data_operasi = ['title' => 'Gaji Bulanan - Rekap'];
+
       foreach (URL::cabang_list_id as $cbi) {
-         //OPERASI
          $join_where = "operasi.id_penjualan = sale_" . $cbi . ".id_penjualan";
-         $where = "sale_" . $cbi . ".bin = 0 AND operasi.insertTime LIKE '" . $date . "%'";
+         $where = "sale_" . $cbi . ".bin = 0 AND operasi.id_user_operasi = " . $user['id'] . " AND operasi.insertTime LIKE '" . $date . "%'";
          $data_lain1 = $this->db(1)->innerJoin1_where('operasi', 'sale_' . $cbi, $join_where, $where);
          foreach ($data_lain1 as $dl1) {
             array_push($data_main, $dl1);
          }
 
-         //PENERIMAAN
          $cols = "id_user, id_cabang, COUNT(id_user) as terima";
-         $where = "insertTime LIKE '" . $date . "%' GROUP BY id_user, id_cabang";
+         $where = "id_user = " . $user['id'] . " AND  insertTime LIKE '" . $date . "%' GROUP BY id_user, id_cabang";
          $data_lain2 = $this->db(1)->get_cols_where('sale_' . $cbi, $cols, $where, 1);
          foreach ($data_lain2 as $dl2) {
             array_push($data_terima, $dl2);
          }
 
-         //PENGAMBILAN
          $cols = "id_user_ambil, id_cabang, COUNT(id_user_ambil) as kembali";
-         $where = "tgl_ambil LIKE '" . $date . "%' GROUP BY id_user_ambil, id_cabang";
+         $where = "id_user_ambil = " . $user['id'] . " AND tgl_ambil LIKE '" . $date . "%' GROUP BY id_user_ambil, id_cabang";
          $data_lain3 = $this->db(1)->get_cols_where('sale_' . $cbi, $cols, $where, 1);
          foreach ($data_lain3 as $dl3) {
             array_push($data_kembali, $dl3);
