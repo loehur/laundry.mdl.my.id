dd0658f34a9ca4c2265df385fd6569dd8c3517b1
loehur <loehur@gmail.com>
2024-09-18 21:29:55 +0700

    benerin df3e3s2432

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -4,48 +4,61 @@ class Cron extends Controller
 {
    public function send()
    {
+      $pending = 0;
+      $expired = 0;
+      $sent = 0;
       $where = "proses = '' AND token <> '' AND status <> 5 AND id_api = '' ORDER BY insertTime ASC";
-      $data = $this->db(1)->get_where('notif_' . $this->id_cabang, $where);
 
-      foreach ($data as $dm) {
-         $id_notif = $dm['id_notif'];
+      foreach (URL::cabang_list_id as $cli) {
+         $data = $this->db(1)->get_where('notif_' . $cli, $where);
+         $pending += count($data);
+         foreach ($data as $dm) {
+            $id_notif = $dm['id_notif'];
 
-         $expired = false;
+            $expired = false;
 
-         $t1 = strtotime($dm['insertTime']);
-         $t2 = strtotime(date("Y-m-d H:i:s"));
-         $diff = $t2 - $t1;
-         $hours = round($diff / (60 * 60), 1);
+            $t1 = strtotime($dm['insertTime']);
+            $t2 = strtotime(date("Y-m-d H:i:s"));
+            $diff = $t2 - $t1;
+            $hours = round($diff / (60 * 60), 1);
 
-         if ($hours > 15) {
-            $expired = true;
-         }
+            if ($hours > 15) {
+               $expired = true;
+            }
 
-         if ($expired == false) {
-            $hp = $dm['phone'];
-            $text = $dm['text'];
-            $token = $dm['token'];
-            $res = $this->model("M_WA")->send($hp, $text, $token);
+            if ($expired == false) {
+               $hp = $dm['phone'];
+               $text = $dm['text'];
+               $token = $dm['token'];
+               $res = $this->model("M_WA")->send($hp, $text, $token);
 
-            if (isset($res['id'])) {
-               foreach ($res['id'] as $v) {
-                  $status = $res["process"];
-                  $set = "status = 1, proses = '" . $status . "', id_api = '" . $v . "'";
-                  $where2 = "id_notif = '" . $id_notif . "'";
-                  $this->db(1)->update('notif_' . $this->id_cabang, $set, $where2);
+               if (isset($res['id'])) {
+                  foreach ($res['id'] as $v) {
+                     $status = $res["process"];
+                     $set = "status = 1, proses = '" . $status . "', id_api = '" . $v . "'";
+                     $where2 = "id_notif = '" . $id_notif . "'";
+                     $this->db(1)->update('notif_' . $cli, $set, $where2);
+                  }
+                  $sent += 1;
+               } else {
+                  continue;
                }
             } else {
-               continue;
+               $status = "expired";
+               $set = "status = 2, proses = '" . $status . "'";
+               $where2 = "id_notif = '" . $id_notif . "'";
+               $this->db(1)->update('notif_' . $cli, $set, $where2);
+               $expired += 1;
             }
-         } else {
-            $status = "expired";
-            $set = "status = 2, proses = '" . $status . "'";
-            $where2 = "id_notif = '" . $id_notif . "'";
-            $this->db(1)->update('notif_' . $this->id_cabang, $set, $where2);
-         }
 
+            sleep(1);
+         }
          sleep(1);
       }
+
+      echo "Notif Pending: " . $pending;
+      echo "<br>Notif Expired: " . $expired;
+      echo "<br>Notif Sent: " . $sent;
    }
 
    public function cek()
