5100717847da26ec506904462c82a5aa416d5472
loehur <loehur@gmail.com>
2024-09-23 21:01:06 +0700

    rusak2233s323333

--- app/Views/operasi/view_load.php
+++ app/Views/operasi/view_load.php
@@ -281,7 +281,7 @@ $labeled = false;
 
                 $buttonNotifSelesai = "";
                 if ($b == $endLayanan && $endLayananDone == true) {
-                  foreach ($data['notif_penjualan'] as $notif) {
+                  foreach ($data['notif_selesai'] as $notif) {
                     if ($notif['no_ref'] == $id) {
                       $stNotif = "<b>" . ucwords($notif['proses']) . "</b> " . ucwords($notif['state']);
                       $buttonNotifSelesai = "<small><span><b><i class='fab fa-whatsapp'></i></b> " . ucwords($stNotif) . "</span></small><br>";
