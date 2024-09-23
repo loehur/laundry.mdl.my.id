a136af0de4855e85904312ffd56df07d403a4b40
loehur <loehur@gmail.com>
2024-09-23 20:38:53 +0700

    rusak2233s3

--- app/Views/operasi/view_load.php
+++ app/Views/operasi/view_load.php
@@ -43,6 +43,67 @@
   </div>
 </form>
 
+<form data-operasi="" class="operasi ajax" action="<?= URL::BASE_URL; ?>Antrian/operasi" method="POST">
+  <div class="modal" id="exampleModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Selesai <b class='operasi'></b>!</h5>
+        </div>
+        <div class="modal-body">
+          <div class="card-body">
+            <div class="form-group">
+              <div class="row">
+                <div class="col">
+                  <label>Karyawan</label>
+                  <select name="f1" class="operasi form-control tize form-control-sm userChange" style="width: 100%;" required>
+                    <option value="" selected disabled></option>
+                    <optgroup label="<?= $this->dCabang['nama'] ?> [<?= $this->dCabang['kode_cabang'] ?>]">
+                      <?php foreach ($this->user as $a) { ?>
+                        <option id="<?= $a['id_user'] ?>" value="<?= $a['id_user'] ?>"><?= $a['id_user'] . "-" . strtoupper($a['nama_user']) ?></option>
+                      <?php } ?>
+                    </optgroup>
+                    <?php if (count($this->userCabang) > 0) { ?>
+                      <optgroup label="----- Cabang Lain -----">
+                        <?php foreach ($this->userCabang as $a) { ?>
+                          <option id="<?= $a['id_user'] ?>" value="<?= $a['id_user'] ?>"><?= $a['id_user'] . "-" . strtoupper($a['nama_user']) ?></option>
+                        <?php } ?>
+                      </optgroup>
+                    <?php } ?>
+                  </select>
+                </div>
+                <div class="col">
+                  <label>Letak / Rak</label>
+                  <input id='letakRAK' type="text" maxlength="2" name="rak" style="text-transform: uppercase" class="form-control">
+                </div>
+              </div>
+              <input type="hidden" class="idItem" name="f2" value="" required>
+              <input type="hidden" class="valueItem" name="f3" value="" required>
+              <input type="hidden" class="textNotif" name="text" value="" required>
+              <input type="hidden" class="hpNotif" name="hp" value="" required>
+            </div>
+            <div class="form-group letakRAK">
+              <div class="row">
+                <div class="col">
+                  <label>Pack</label>
+                  <input type="number" min="0" value="1" name="pack" style="text-transform: uppercase" class="form-control" required>
+                </div>
+                <div class="col">
+                  <label>Hanger</label>
+                  <input type="number" min="0" value="0" name="hanger" style="text-transform: uppercase" class="form-control" required>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-sm btn-primary">Selesai</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
+
 <?php
 
 $kodeCabang = $this->dCabang['kode_cabang'];
@@ -1380,67 +1441,6 @@ $no_pelanggan = $data['pelanggan']['nomor'];
       </div>
     </div>
   <?php } ?>
-
-  <form data-operasi="" class="operasi ajax" action="<?= URL::BASE_URL; ?>Antrian/operasi" method="POST">
-    <div class="modal" id="exampleModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Selesai <b class='operasi'></b>!</h5>
-          </div>
-          <div class="modal-body">
-            <div class="card-body">
-              <div class="form-group">
-                <div class="row">
-                  <div class="col">
-                    <label>Karyawan</label>
-                    <select name="f1" class="operasi form-control tize form-control-sm userChange" style="width: 100%;" required>
-                      <option value="" selected disabled></option>
-                      <optgroup label="<?= $this->dCabang['nama'] ?> [<?= $this->dCabang['kode_cabang'] ?>]">
-                        <?php foreach ($this->user as $a) { ?>
-                          <option id="<?= $a['id_user'] ?>" value="<?= $a['id_user'] ?>"><?= $a['id_user'] . "-" . strtoupper($a['nama_user']) ?></option>
-                        <?php } ?>
-                      </optgroup>
-                      <?php if (count($this->userCabang) > 0) { ?>
-                        <optgroup label="----- Cabang Lain -----">
-                          <?php foreach ($this->userCabang as $a) { ?>
-                            <option id="<?= $a['id_user'] ?>" value="<?= $a['id_user'] ?>"><?= $a['id_user'] . "-" . strtoupper($a['nama_user']) ?></option>
-                          <?php } ?>
-                        </optgroup>
-                      <?php } ?>
-                    </select>
-                  </div>
-                  <div class="col">
-                    <label>Letak / Rak</label>
-                    <input id='letakRAK' type="text" maxlength="2" name="rak" style="text-transform: uppercase" class="form-control">
-                  </div>
-                </div>
-                <input type="hidden" class="idItem" name="f2" value="" required>
-                <input type="hidden" class="valueItem" name="f3" value="" required>
-                <input type="hidden" class="textNotif" name="text" value="" required>
-                <input type="hidden" class="hpNotif" name="hp" value="" required>
-              </div>
-              <div class="form-group letakRAK">
-                <div class="row">
-                  <div class="col">
-                    <label>Pack</label>
-                    <input type="number" min="0" value="1" name="pack" style="text-transform: uppercase" class="form-control" required>
-                  </div>
-                  <div class="col">
-                    <label>Hanger</label>
-                    <input type="number" min="0" value="0" name="hanger" style="text-transform: uppercase" class="form-control" required>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-sm btn-primary">Selesai</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </form>
 
   <form class="operasi ajax" action="<?= URL::BASE_URL; ?>Operasi/ganti_operasi" method="POST">
     <div class="modal" id="modalGanti">
