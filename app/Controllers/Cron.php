3c6075b4ca604f897954966c3388451fd24878dc
loehur <loehur@gmail.com>
2024-09-27 21:15:29 +0700

    EXPIRED WA JADI 24 JAM33

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -5,7 +5,7 @@ class Cron extends Controller
    public function send()
    {
       $pending = 0;
-      $expired = 0;
+      $expire = 0;
       $sent = 0;
       $where = "proses = '' AND token <> '' AND status <> 5 AND id_api = '' ORDER BY insertTime ASC";
 
@@ -15,7 +15,7 @@ class Cron extends Controller
          foreach ($data as $dm) {
             $id_notif = $dm['id_notif'];
 
-            $expired = false;
+            $expired_bol = false;
 
             $t1 = strtotime($dm['insertTime']);
             $t2 = strtotime(date("Y-m-d H:i:s"));
@@ -23,10 +23,10 @@ class Cron extends Controller
             $hours = round($diff / (60 * 60), 1);
 
             if ($hours > 24) {
-               $expired = true;
+               $expired_bol = true;
             }
 
-            if ($expired == false) {
+            if ($expired_bol == false) {
                $hp = $dm['phone'];
                $text = $dm['text'];
                $token = $dm['token'];
@@ -48,66 +48,12 @@ class Cron extends Controller
                $set = "status = 2, proses = '" . $status . "'";
                $where2 = "id_notif = '" . $id_notif . "'";
                $this->db(1)->update('notif_' . $cli, $set, $where2);
-               $expired += 1;
+               $expire += 1;
             }
          }
       }
 
-      echo "Pending: " . $pending . " \nExpired: " . $expired . " \nSent: " . $sent . "\n";
-   }
-
-   public function send_not_ready()
-   {
-      $pending = 0;
-      $expired = 0;
-      $sent = 0;
-      $where = "proses = '' AND token <> '' AND id_api = '' ORDER BY insertTime ASC";
-
-      foreach (URL::cabang_list_id as $cli) {
-         $data = $this->db(1)->get_where('notif_' . $cli, $where);
-         $pending += count($data);
-         foreach ($data as $dm) {
-            $id_notif = $dm['id_notif'];
-
-            $expired = false;
-
-            $t1 = strtotime($dm['insertTime']);
-            $t2 = strtotime(date("Y-m-d H:i:s"));
-            $diff = $t2 - $t1;
-            $hours = round($diff / (60 * 60), 1);
-
-            if ($hours > 15) {
-               $expired = true;
-            }
-
-            if ($expired == false) {
-               $hp = $dm['phone'];
-               $text = $dm['text'];
-               $token = $dm['token'];
-               $res = $this->model("M_WA")->send($hp, $text, $token);
-
-               if (isset($res['id'])) {
-                  foreach ($res['id'] as $v) {
-                     $status = $res["process"];
-                     $set = "status = 1, proses = '" . $status . "', id_api = '" . $v . "'";
-                     $where2 = "id_notif = '" . $id_notif . "'";
-                     $this->db(1)->update('notif_' . $cli, $set, $where2);
-                  }
-                  $sent += 1;
-               } else {
-                  continue;
-               }
-            } else {
-               $status = "expired";
-               $set = "status = 2, proses = '" . $status . "'";
-               $where2 = "id_notif = '" . $id_notif . "'";
-               $this->db(1)->update('notif_' . $cli, $set, $where2);
-               $expired += 1;
-            }
-         }
-      }
-      echo "FORCE send not ready whatsapp\n";
-      echo "Pending: " . $pending . " \nExpired: " . $expired . " \nSent: " . $sent . "\n";
+      echo "Pending: " . $pending . " \nExpired: " . $expire . " \nSent: " . $sent . "\n";
    }
 
    function pay_bill()
