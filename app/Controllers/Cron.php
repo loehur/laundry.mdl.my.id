b48d0611de9e530138105e6b3a96d24339018a95
loehur <loehur@gmail.com>
2024-09-27 20:52:32 +0700

    send notif pelanggan23334

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -115,6 +115,23 @@ class Cron extends Controller
    {
       //cek semua tagihan
       $month = date('Ym');
+      $week = 1;
+      $thi = date('d');
+      if ($thi > 0 && $thi <= 5) {
+         $week = 1;
+      } else if ($thi > 5 && $thi <= 10) {
+         $week = 2;
+      } else if ($thi > 10 && $thi <= 15) {
+         $week = 3;
+      } else if ($thi > 15 && $thi <= 20) {
+         $week = 4;
+      } else if ($thi > 20 && $thi <= 25) {
+         $week = 5;
+      } else {
+         $week = 6;
+      }
+      $month .= $week;
+
       $data = $this->db(0)->get('postpaid_list');
       foreach ($data as $dt) {
          $code = $dt['code'];
