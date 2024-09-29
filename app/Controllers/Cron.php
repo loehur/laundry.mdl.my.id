15681b0268a518e36b8e65704e2184db030f5941
loehur <loehur@gmail.com>
2024-09-29 12:08:05 +0700

    cron pay fixed3d3

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -76,26 +76,30 @@ class Cron extends Controller
          $rc = isset($d['response_code']) ? $d['response_code'] : $a['response_code'];
          $balance = isset($d['balance']) ? $d['balance'] : $a['balance'];
          $price = isset($d['price']) ? $d['price'] : $a['price'];
-
-         if ($rc == '17') {
-            $alert = "Not Enough Balance to pay " . $dt['description'] . " Rp" . number_format($price);
-            echo $alert . "\n";
-            $res = $this->model("M_WA")->send(URL::WA_ADMIN, $alert, URL::WA_TOKEN);
-            if (!isset($res["id"])) {
-               if (isset($res['reason'])) {
-                  $msg .= "Whatsapp Error, " . $res['reason'] . "\n";
-               } else {
-                  $msg .= "Whatsapp Error, Sending Failed\n";
-               }
-            }
-            exit();
-         }
-
          $message = isset($d['message']) ? $d['message'] : $a['message'];
          $tr_id = isset($d['tr_id']) ? $d['tr_id'] : $a['tr_id'];
          $datetime = isset($d['datetime']) ? $d['datetime'] : $a['datetime'];
          $noref = isset($d['noref']) ? $d['noref'] : $a['noref'];
          $tr_status = isset($d['status']) ? $d['status'] : 3;
+
+         switch ($rc) {
+            case '17':
+               $alert = "Not Enough Balance to pay " . $dt['description'] . " Rp" . number_format($price);
+               echo $alert . "\n";
+               $res = $this->model("M_WA")->send(URL::WA_ADMIN, $alert, URL::WA_TOKEN);
+               if (!isset($res["id"])) {
+                  if (isset($res['reason'])) {
+                     $msg .= "Whatsapp Error, " . $res['reason'] . "\n";
+                  } else {
+                     $msg .= "Whatsapp Error, Sending Failed\n";
+                  }
+               }
+               exit();
+               break;
+            case '04':
+               $tr_status = 2;
+               break;
+         }
 
          if ($tr_status == 1) {
             $where = "customer_id = '" . $d['hp'] . "' AND code = '" . $d['code'] . "'";
@@ -123,7 +127,7 @@ class Cron extends Controller
          $set =  "tr_status = " . $tr_status . ", datetime = '" . $datetime . "', noref = '" . $noref . "', price = " . $price . ", message = '" . $message . "', balance = " . $balance . ", tr_id = '" . $tr_id . "', response_code = '" . $rc . "'";
          $update = $this->db(0)->update('postpaid', $set, $where);
          if ($update['errno'] == 0) {
-            $msg .= $dt['description'] . " " . $a['message'] . "\n";
+            $msg .= $dt['description'] . " - PAY - " . $a['message'] . "\n";
          } else {
             $alert = "DB Error " . $update['error'];
             $msg .= $alert . "\n";
@@ -229,7 +233,7 @@ class Cron extends Controller
 
    function pay_bill()
    {
-      //cek semua tagihan
+      //CEK SEMUA TAGIHAN
       $month = date('Ym');
       $week = 1;
       $thi = date('d');
@@ -259,7 +263,7 @@ class Cron extends Controller
          }
 
          //cek tagihan yg udah pernah di cek atau dibayar
-         $where = "customer_id = '" . $dt['customer_id'] . "' AND code = '" . $dt['code'] . "' AND (tr_status = 3 OR tr_status = 4)";
+         $where = "customer_id = '" . $dt['customer_id'] . "' AND code = '" . $dt['code'] . "' AND (tr_status = 0 OR tr_status = 3)";
          $cek = $this->db(0)->get_where('postpaid', $where);
          if (count($cek) > 0) {
             foreach ($cek as $a) {
@@ -308,10 +312,10 @@ class Cron extends Controller
                      case "39":
                      case "201":
                         $col = "response_code, message, tr_id, tr_name, period, nominal, admin, ref_id, code, customer_id, price, selling_price, description, tr_status, id_cabang";
-                        $val = "'" . $d['response_code'] . "','" . $d['message'] . "'," . $d['tr_id'] . ",'" . $d['tr_name'] . "','" . $d['period'] . "'," . $d['nominal'] . "," . $d['admin'] . ",'" . $d['ref_id'] . "','" . $d['code'] . "','" . $d['hp'] . "'," . $d['price'] . "," . $d['selling_price'] . ",'" . serialize($d['desc']) . "',4," . $dt['id_cabang'];
+                        $val = "'" . $d['response_code'] . "','" . $d['message'] . "'," . $d['tr_id'] . ",'" . $d['tr_name'] . "','" . $d['period'] . "'," . $d['nominal'] . "," . $d['admin'] . ",'" . $d['ref_id'] . "','" . $d['code'] . "','" . $d['hp'] . "'," . $d['price'] . "," . $d['selling_price'] . ",'" . serialize($d['desc']) . "',0," . $dt['id_cabang'];
                         $do = $this->db(0)->insertCols("postpaid", $col, $val);
                         if ($do['errno'] == 0) {
-                           echo $dt['description'] . " " . $d['message'] . "\n";
+                           echo $dt['description'] . " - CHECK - " . $d['message'] . "\n";
 
                            //bayar karna sudah pernah di cek
                            $where = "ref_id = '" . $d['ref_id'] . "'";
@@ -331,6 +335,7 @@ class Cron extends Controller
                         }
                         break;
                      case 106:
+                        //PRIVIDER GANGGUAN
                         if (isset($d['message'])) {
                            $alert = $dt['description'] . " - " . $d['message'];
                         } else {
@@ -345,7 +350,6 @@ class Cron extends Controller
                               echo "Whatsapp Error, Sending Failed\n";
                            }
                         }
-                        break;
                         break;
                      default:
                         if (isset($d['message'])) {
