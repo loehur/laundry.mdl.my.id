f52830f18d9f848779dc625015ae54248adb9e6e
loehur <loehur@gmail.com>
2024-10-07 14:17:26 +0700

    rapi2 login2ss32

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -805,17 +805,4 @@ if ($log_mode == 1) {
                         },
                     });
                 });
-
-                var time = new Date().getTime();
-                $(document.body).bind("mousemove keypress", function(e) {
-                    time = new Date().getTime();
-                });
-
-                function refresh() {
-                    if (new Date().getTime() - time >= 420000)
-                        window.location.reload(true);
-                    else
-                        setTimeout(refresh, 10000);
-                }
-                setTimeout(refresh, 10000);
             </script>
