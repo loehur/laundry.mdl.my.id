173ac0c098e7f30446e74a4bafce331353c033c6
loehur <loehur@gmail.com>
2024-11-12 15:24:56 +0700

    new era database base on year ok2

--- app/Controllers/Antrian.php
+++ app/Controllers/Antrian.php
@@ -416,7 +416,7 @@ class Antrian extends Controller
       $textSaldo = "";
       $where = $this->wCabang . " AND bin = 0 AND id_pelanggan = " . $idPelanggan . " GROUP BY id_harga";
       $cols = "id_harga, SUM(qty) as saldo";
-      $data = $this->db($_SESSION['user']['book'])->get_cols_where('member', $cols, $where, 1);
+      $data = $this->db(0)->get_cols_where('member', $cols, $where, 1);
 
       foreach ($data as $a) {
          $saldoPengurangan = 0;
