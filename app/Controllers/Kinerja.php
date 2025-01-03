26f5bbdebd650cec47269f287d51a02407b8aa5d
loehur <loehur@gmail.com>
2025-01-03 19:32:39 +0700

    oke ini3wsssswss22

--- app/Controllers/Kinerja.php
+++ app/Controllers/Kinerja.php
@@ -53,6 +53,7 @@ class Kinerja extends Controller
          array_push($data_main, $dl1);
       }
 
+      echo $ops_data;
       if (count($ops_data) > 0) {
          //PENJUALAN TAHUN LALU
          foreach ($ops_data as $od) {
