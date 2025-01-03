63cd8843f67b524f67f6c67260042e463e17543c
loehur <loehur@gmail.com>
2025-01-03 19:30:13 +0700

    oke ini3wsssswss2

--- app/Controllers/Kinerja.php
+++ app/Controllers/Kinerja.php
@@ -58,7 +58,7 @@ class Kinerja extends Controller
          foreach ($ops_data as $od) {
             $where = "id_penjualan = " . $od['id_penjualan'];
             $data_lalu = $this->db($_SESSION['user']['book'] - 1)->get_where_row('sale', $where);
-            $new_data = array_merge($od, $data_lalu);
+            $new_data = array_merge($data_lalu, $od);
             array_push($data_main, $new_data);
          }
       }
