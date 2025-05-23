e6d10ed46ac56df78060438547787c61af88c0ef
loehur <loehur@gmail.com>
2025-05-23 20:04:20 +0700

    rapi2 admin aprovalr

--- app/Views/admin_approval/admin_approval_main.php
+++ app/Views/admin_approval/admin_approval_main.php
@@ -2,33 +2,31 @@
 $array = array(0 => 'Setoran', 1 => 'NonTunai', 2 => 'HapusOrder', 3 => 'HapusDeposit', 4 => 'Pengeluaran')
 ?>
 
-<div class="content">
-    <div class="container-fluid">
-        <div class="row border-bottom pb-2" style="max-width: 747px;">
-            <?php
-            $classActive = "";
-            foreach ($array as $a) { ?>
-                <div class="col" style="white-space: nowrap;">
-                    <?php $count = count($data[$a]);
-                    $classActive = ($a == $data['mode']) ? "bg-white" : "";
-                    ?>
-                    <a href="<?= URL::BASE_URL ?>AdminApproval/index/<?= $a ?>" class="border rounded pb-2 <?= $classActive ?>">
-                        <?php if ($count > 0) { ?>
-                            <h6 class="m-0 btn"><?= $a ?> <span class="badge badge-danger"><?= $count ?></span></h6>
-                        <?php } else { ?>
-                            <h6 class="m-0 btn"><?= $a ?> <i class="text-success fas fa-check-circle"></i></span></h6>
-                        <?php } ?>
-                    </a>
-                </div>
-            <?php }
+<div class="row mx-2 border-bottom pb-2">
+    <?php
+    $classActive = "";
+    foreach ($array as $a) { ?>
+        <div class="col-auto px-1" style="white-space: nowrap;">
+            <?php $count = count($data[$a]);
+            $classActive = ($a == $data['mode']) ? "bg-white" : "";
             ?>
+            <a href="<?= URL::BASE_URL ?>AdminApproval/index/<?= $a ?>" class="border rounded pb-2 <?= $classActive ?>">
+                <?php if ($count > 0) { ?>
+                    <h6 class="m-0 btn"><?= $a ?> <span class="badge badge-danger"><?= $count ?></span></h6>
+                <?php } else { ?>
+                    <h6 class="m-0 btn"><?= $a ?> <i class="text-success fas fa-check-circle"></i></span></h6>
+                <?php } ?>
+            </a>
         </div>
-        <div class="row">
-            <div class="col pt-1" id="load">
-            </div>
-        </div>
+    <?php }
+    ?>
+</div>
+
+<div class="row mx-0">
+    <div class="col px-2 pt-1" id="load" style="max-width: 740px;">
     </div>
 </div>
+
 
 <script>
     $(document).ready(function() {
