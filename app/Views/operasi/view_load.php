2106a7f576ed61c207ba73ab87a1de5a6dc30e91
loehur <loehur@gmail.com>
2025-07-16 08:03:27 +0700

    tes445544

--- app/Views/operasi/view_load.php
+++ app/Views/operasi/view_load.php
@@ -829,9 +829,6 @@ $labeled = false;
     </div>
   <?php } ?>
 
-  <pre>
-    <?php print_r($data['kas_member']) ?>
-  </pre>
   <!-- MEMEBR ================================================== -->
 
   <?php
