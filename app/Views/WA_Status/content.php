d277c4a386573b42ddd56273ceec7f128fc1255a
loehur <loehur@gmail.com>
2025-07-20 11:34:02 +0700

    session45455555555554555

--- app/Views/WA_Status/content.php
+++ app/Views/WA_Status/content.php
@@ -10,34 +10,3 @@
     <i class="far fa-times-circle text-danger"></i> Server Down
   <?php } ?>
 </div>
-
-<div class="text-nowrap mt-3">
-  <?php $d = $data[1] ?>
-  <b>Fonnte</b><br>
-  <?php if (isset($d['device'])) { ?>
-    <table class="">
-      <tr>
-        <td class="">Status</td>
-        <td class="">: <?= $d['device_status'] ?></td>
-      </tr>
-      <tr>
-        <td class="">Device</td>
-        <td class="">: <?= $d['device'] ?></td>
-      </tr>
-      <tr>
-        <td class="">Name</td>
-        <td class="">: <?= $d['name'] ?></td>
-      </tr>
-      <tr>
-        <td class="">Expired</td>
-        <td class="">: <?= $d['expired'] ?></td>
-      </tr>
-      <tr>
-        <td class="">Quota</td>
-        <td class="">: <?= $d['quota'] ?></td>
-      </tr>
-    </table>
-  <?php } else { ?>
-    <?php print_r(json_encode($d)) ?>
-  <?php } ?>
-</div>
--- app/Views/WA_Status/loader.php
+++ app/Views/WA_Status/loader.php
@@ -1,11 +1,14 @@
 <div class="content mt-2">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <div class="card p-2" id="load">
-          <div class="text-nowrap text-center">
-            <i class="fas fa-spinner text-warning"></i> Loading...
-          </div>
+  <div class="row mx-0">
+    <div class="col">
+      <div class="card p-2" id="load">
+        <div class="text-nowrap text-center">
+          <i class="fas fa-spinner text-warning"></i> Loading...
+        </div>
+      </div>
+      <div class="card p-2" id="load2">
+        <div class="text-nowrap text-center">
+          <i class="fas fa-spinner text-warning"></i> Loading...
         </div>
       </div>
     </div>
@@ -15,6 +18,8 @@
 <script>
   $(document).ready(function() {
     reload();
+
+    $("div#load2").load("<?= URL::BASE_URL ?>WA_Status/content2");
 
     function reload() {
       $("div#load").load("<?= URL::BASE_URL ?>WA_Status/content");
