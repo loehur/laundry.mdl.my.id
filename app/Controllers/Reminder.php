eea8bf91a553f7340c6445a0f5b2b9f2b5d745fe
loehur <loehur@gmail.com>
2025-08-13 09:56:30 +0700

    reminder new

--- app/Controllers/Reminder.php
+++ app/Controllers/Reminder.php
@@ -4,33 +4,36 @@ class Reminder extends Controller
 {
    public function cek()
    {
-      $where = "DATE(NOW()) >= (next_date - INTERVAL 7 DAY)";
-      $data = $this->db(0)->get_where('reminder', $where);
+      $data = $this->db(0)->get('reminder');
       foreach ($data as $d) {
          $t1 = strtotime($d['next_date']);
          $t2 = strtotime(date("Y-m-d H:i:s"));
          $diff = $t1 - $t2;
-         $dates = floor(($diff / (60 * 60)) / 24);
+         $selisih_hari = floor(($diff / (60 * 60)) / 24);
 
-         if ($dates > 0) {
-            $text_count = $dates . " Hari Lagi";
-         } elseif ($dates < 0) {
-            $text_count = "Terlewat " . $dates * -1 . " Hari";
-         } else {
-            $text_count = "Hari Ini";
+         $rentang = $d['range'];
+
+         if ($selisih_hari <= $rentang) {
+            if ($selisih_hari > 0) {
+               $text_count = $selisih_hari . " Hari Lagi";
+            } elseif ($selisih_hari < 0) {
+               $text_count = "Terlewat " . $selisih_hari * -1 . " Hari";
+            } else {
+               $text_count = "Hari Ini";
+            }
+
+            $note = "";
+            if ($d['note'] <> "") {
+               $note = "\n" . $d['note'];
+            }
+
+            $ops_link = URL::HOST_URL . "/I/r/" . $d['id'];
+            $hp = $d['notif_number'];
+            $text = "*" . $d['name'] . "* " . $note . " \n" . $text_count . " \n" . $ops_link;
+            echo $d['name'] . " " . $text_count . " \n";
+
+            $res = $this->data('Notif')->send_wa($hp, $text);
          }
-
-         $link = "";
-         if ($d['link'] <> "") {
-            $link = "\n" . $d['link'];
-         }
-
-         $ops_link = URL::HOST_URL . "/I/r/" . $d['id'];
-         $hp = $d['notif_number'];
-         $text = "*" . $d['name'] . "* " . $link . " \n" . $text_count . " \n" . $ops_link;
-         echo $d['name'] . " " . $text_count . " \n";
-
-         $res = $this->data('Notif')->send_wa($hp, $text);
       }
    }
 
@@ -38,16 +41,17 @@ class Reminder extends Controller
    {
       $id = $_POST['id'];
       $where = "id = " . $id;
-      $data = $this->db(0)->get_where_row('reminder', $where);
-      $cycle = $data['cycle'];
+      $d = $this->db(0)->get_where_row('reminder', $where);
+      $cycle = $d['cycle'];
 
-      $t1 = strtotime($data['next_date']);
+      $t1 = strtotime($d['next_date']);
       $t2 = strtotime(date("Y-m-d H:i:s"));
       $diff = $t1 - $t2;
-      $dates = floor(($diff / (60 * 60)) / 24);
+      $selisih_hari = floor(($diff / (60 * 60)) / 24);
+      $rentang = $d['range'];
 
-      if ($dates <= 7) {
-         $next_date = date("Y-m-d", strtotime($data['next_date'] . " +" . $cycle . " month"));
+      if ($selisih_hari <= $rentang) {
+         $next_date = date("Y-m-d", strtotime($d['next_date'] . " +" . $cycle . " " . $d['cycle_type']));
          $up = $this->db(0)->update('reminder', "next_date = '" . $next_date . "'", $where);
          if ($up['errno'] == 0) {
             echo 0;
