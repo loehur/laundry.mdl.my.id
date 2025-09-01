504b5c921114e29fd9bf25f2489186b55b81e314
loehur <loehur@gmail.com>
2025-09-01 19:46:58 +0700

    cashbon error533

--- app/Data/D_Gaji.php
+++ app/Data/D_Gaji.php
@@ -128,7 +128,7 @@ class D_Gaji extends Controller
 
         foreach ($data as $key => $k) {
             $ref = $k['id_kas'];
-            $where = "ref = '" . $ref . "'";
+            $where = "ref = '" . $ref . "' AND id_karyawan = " . $userID . " AND tgl LIKE '" . substr($month, 0, 4) . "%'";
             $countPotong = $this->db(0)->count_where('gaji_result', $where);
             if ($countPotong == 1) {
                 unset($data[$key]);
