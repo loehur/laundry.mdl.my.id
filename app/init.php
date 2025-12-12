facbe6620e79327e8854df7e78bb988eac8dffcd
loehur <loehur@gmail.com>
2025-12-12 20:00:34 +0700

    fix payment api66666666666y656667r6

--- app/init.php
+++ app/init.php
@@ -4,5 +4,12 @@ date_default_timezone_set("Asia/Jakarta");
 $GLOBALS['now'] = date("Y-m-d H:i:s");
 
 spl_autoload_register(function ($class) {
-     require_once 'Core/' . $class . '.php';
+     $corePath = 'Core/' . $class . '.php';
+     $modelPath = 'Models/' . $class . '.php';
+     
+     if (file_exists($corePath)) {
+          require_once $corePath;
+     } elseif (file_exists($modelPath)) {
+          require_once $modelPath;
+     }
 });
